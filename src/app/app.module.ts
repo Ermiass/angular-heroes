47fd6b45c83b55067f85b426aced80d82a3d3a83
Ermiass <Erohailu@gmail.com>
2023-09-24 14:33:48 -0400

    (update:new branch created)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
+import { UpdateComponent } from './heroes/new/update/update.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeroesComponent
+    HeroesComponent,
+    UpdateComponent
   ],
   imports: [
     BrowserModule,
