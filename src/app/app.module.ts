868ecaedf8178aed82316edc94761d1d0dbfd228
Ermiass <Erohailu@gmail.com>
2023-10-29 22:57:36 -0400

    (new:routing )

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,17 @@ import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import { UpdateComponent } from './heroes/new/update/update.component';
 import { RoutesComponent } from './heroes/new/routes/routes.component';
+import { ImagesComponent } from './assets/images/images.component';
+import { PathComponent } from './new/path/path.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeroesComponent,
     UpdateComponent,
-    RoutesComponent
+    RoutesComponent,
+    ImagesComponent,
+    PathComponent
   ],
   imports: [
     BrowserModule,
